638a5a47407982f17453edb1951b215099118c32
Johanna Snow <johannadsnow@gmail.com>
2017-02-13 15:27:08 -0600

    Greeting started

--- Scripts/client.js
+++ Scripts/client.js
@@ -6,21 +6,24 @@ console.log('The client script is sourced');
     // console.log('companies', companies);
 
 
- function Customer(firstName, lastName, roomNumber, startTimestamp, endTimestamp) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.reservation = {
+ function Customer(firstName, lastName, roomNumber, startTimestamp, endTimestamp) { //Class
+  this.firstName = firstName; ////property of customer
+  this.lastName = lastName; /////property of customer
+  this.reservation = {   /// property of customer
     roomNumber,
     startTimestamp,
     endTimestamp
   };
   this.greeting = function() {
-    alert('Good morning ' + this.firstName + ', and welcome to ' + this.company + 'Room ' + this.roomNumber + 'is now ready for you. Enjoy your stay, and let us know if you need anything.');
+    ('Good morning, ' + this.firstName);
+    console.log('greeting')
   };
 }
   var customer1 = new Customer('Joe', 'Schmoe', '403', '12345', '55432');
   customer1.greeting();
 console.log( 'customer1', customer1);
+
+
 // customer1.greeting = function() {
 //   alert('Good morning ' + this.firstName + ', and welcome to ' + this.company + 'Room ' + this.roomNumber + 'is now ready for you. Enjoy your stay, and let us know if you need anything.');
 // };
