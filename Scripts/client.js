770260568a89bbe45b7f5d46ddaff095354682f0
Johanna Snow <johannadsnow@gmail.com>
2017-02-13 18:50:15 -0600

    If/Else Time Works yaaaassssss

--- Scripts/client.js
+++ Scripts/client.js
@@ -1,19 +1,16 @@
-console.log('The client script is sourced');
-// var myDate = new Date();
     //
     // var guests = require('./guests.json');
     // console.log('guests', guests);
     // var companies = require('./companies.json');
     // console.log('companies', companies);
 
-  function Hotel(company, city, timeZone, hotelParameter){
+  function Hotel(company, city, timeZone){
     this.company =company;
     this.city = city;
     this.timeZone = timeZone;
   }
 
-
- function Guest(firstName, lastName, roomNumber, startTimestamp, endTimestamp, hotelParameter) { //Class
+  function Guest(firstName, lastName, roomNumber, startTimestamp, endTimestamp, hotelParameter) { //Class
   this.firstName = firstName; ////property of guest
   this.lastName = lastName; /////property of guest
   this.hotel = hotelParameter;
@@ -25,25 +22,22 @@ console.log('The client script is sourced');
   this.greeting = function() {
     var myDate = new Date();
     /*hour is before noon */
-    if ( myDate.getHours() < 12 )
-{
-   ('Good Morning ' + this.firstName + ', and welcome to ' + this.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
-Console.log('good morning');
-// } else  /* Hour is from noon to 5pm (actually to 5:59 pm) */
-//     if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
-// {
-//     document.write('Good Afternoon, ' + this.firstName + ', and welcome to ' + this.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
-// }  else  /* the hour is after 5pm, so it is between 6pm and midnight */
-//     if ( myDate.getHours() > 17 && myDate.getHours() <= 24 )
-// {
-//     document.write('Good Evening, ' + this.firstName + ', and welcome to ' + this.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
-// } else { /* the hour is not between 0 and 24, so something is wrong */
-//     document.write('Error, time not found');
-//
-    // console.log('greeting')
-//   }
-// };
-
+    if ( myDate.getHours() < 12 ){
+    console.log('myDate.getHours()');
+   ('Good Morning ' + this.firstName + ', and welcome to ' + this.hotel.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
+   console.log('Morning Greeting');
+} else if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 ){
+  /* Hour is from noon to 5pm (actually to 5:59 pm) */
+  ('Good Afternoon, ' + this.firstName + ', and welcome to ' + this.hotel.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
+    console.log('Afternoon Greeting');
+  } else if ( myDate.getHours() > 17 && myDate.getHours() <= 24 ){
+   /* the hour is after 5pm, so it is between 6pm and midnight */
+    ('Good Evening, ' + this.firstName + ', and welcome to ' + this.hotel.company + '!' + 'Room ' + this.reservation.roomNumber + 'is now ready for you! Enjoy your stay, and let us know if you need anything.');
+      console.log('Evening Greeting');
+} else {
+/* the hour is not between 0 and 24, so something is wrong */
+  ('Error, time not found');
+    console.log('Error');
 }
 };
 }
